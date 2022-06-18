52f8dd18e77eff00e6bc4c89c8d4dc357c309e72
nk161690 <khanhndnhe161690@fpt.edu.vn>
2022-06-18 21:02:13 +0700

    unit testing

--- SWP/test/model/DAOProjectTest.java
+++ SWP/test/model/DAOProjectTest.java
@@ -13,7 +13,9 @@ import static org.junit.Assert.*;
  * @author Khanh
  */
 public class DAOProjectTest {
-    
+
+    DAOProject dao = new DAOProject();
+
     public DAOProjectTest() {
     }
 
@@ -21,21 +23,74 @@ public class DAOProjectTest {
      * Test of getProject method, of class DAOProject.
      */
     @Test
-    public void testGetProject() {
+    public void testGetProject1() {
+        assertEquals("[projects{title=pj1, client=2    , start_date=16/06/2022, "
+                + "end_date=05/07/2022, rate=10000.0, manager=ABCDE, "
+                + "description=N/A}, projects{title=pj20, client=1    , "
+                + "start_date=20/06/2022, end_date=30/07/2022, rate=10000.0, "
+                + "manager=ABCDE, description=<p>Demo</p>}, projects{title=pj22, "
+                + "client=1    , start_date=20/06/2022, end_date=30/07/2022, "
+                + "rate=10000.0, manager=ABCDE, description=<p>Demo</p>},"
+                + " projects{title=pj3, client=1    , start_date=15/06/2022,"
+                + " end_date=23/06/2022, rate=0.0, manager=ABCDE, "
+                + "description=<p>demo</p>}, projects{title=pj4, client=1    ,"
+                + " start_date=30/05/2022, end_date=30/06/2022, rate=0.0, "
+                + "manager=ABCDE, description=<p>Demo</p>}, projects{title=pj5, "
+                + "client=1    , start_date=01/06/2022, end_date=08/07/2022, "
+                + "rate=10000.0, manager=ABCDE, description=<p>Demo</p>},"
+                + " projects{title=pj6, client=2    , start_date=15/06/2022, "
+                + "end_date=16/06/2022, rate=1000.0, manager=ABCDE, "
+                + "description=<p>Demo</p>}, projects{title=pj9, client=1    , "
+                + "start_date=15/06/2022, end_date=24/06/2022, rate=10000.0, "
+                + "manager=ABCDE, description=<p>Demo</p>}, "
+                + "projects{title=project_demo, client=1    , start_date=20/06/2022, "
+                + "end_date=30/06/2022, rate=10000.0, manager=ABCDE, description=}, "
+                + "projects{title=Qutsor, client=1    , start_date=20/06/2022,"
+                + " end_date=30/07/2022, rate=10000.0, manager=ABCDE, "
+                + "description=<p>Demo</p>}, projects{title=tiuwq2, client=1    "
+                + ", start_date=20/06/2022, end_date=30/07/2022, rate=10000.0, "
+                + "manager=ABCDE, description=<p>Demo</p>}, projects{title=utusdh, "
+                + "client=1    , start_date=20/06/2022, end_date=30/07/2022, "
+                + "rate=10000.0, manager=ABCDE, description=<p>Demo</p>}]",
+                dao.getProject("ABCDE").toString());
+    }
+
+    @Test
+    public void testgetProject2() {
+        assertEquals("[]", dao.getProject("79852").toString());
     }
 
     /**
      * Test of getP method, of class DAOProject.
      */
     @Test
-    public void testGetP() {
+    public void testGetP1() {
+        assertEquals("projects{title=pj3, client=1    , start_date=15/06/2022,"
+                + " end_date=23/06/2022, rate=0.0, manager=ABCDE, description=<p>demo</p>}",
+                dao.getP("pj3").toString());
+    }
+
+    @Test
+    public void testGetP2() {
+        assertNull(dao.getP("24962"));
     }
 
     /**
      * Test of addProject method, of class DAOProject.
      */
     @Test
-    public void testAddProject() {
+    public void testAddProject1() {
+        //assertTrue(dao.addProject("tiuwq2", "1    ","20/06/2022", "30/07/2022", 10000, "ABCDE", "<p>Demo</p>"));
+    }
+
+    @Test
+    public void testAddProject2() {
+        assertFalse(dao.addProject("tiuwq2", "1    ", "20/06/2022", "30/07/2022", 10000, "ABCDE", "<p>Demo</p>"));
+    }
+
+    @Test
+    public void testAddProject3() {
+        assertFalse(dao.addProject("flhwoifwh", "1    ", "20/06/2022", "30/07/2022", 10000, "yigd", "<p>Demo</p>"));
     }
 
     /**
@@ -43,6 +98,7 @@ public class DAOProjectTest {
      */
     @Test
     public void testDeleteProject() {
+        //assertTrue(dao.deleteProject("pj18"));
     }
 
     /**
@@ -50,20 +106,27 @@ public class DAOProjectTest {
      */
     @Test
     public void testUpdateProject() {
+        //assertTrue(dao.updateProject("project_demo", "1", "20/06/2022", "30/06/2022", 10000, "ABCDE", ""));      
     }
 
     /**
      * Test of search method, of class DAOProject.
      */
     @Test
-    public void testSearch() {
+    public void testSearch1() {
+        assertEquals("[projects{title=project_demo, client=1    , start_date=20/06/2022, "
+                + "end_date=30/06/2022, rate=10000.0, manager=ABCDE, description=}]",
+                dao.search("pro", "ABCDE").toString());
     }
 
-    /**
-     * Test of main method, of class DAOProject.
-     */
     @Test
-    public void testMain() {
+    public void testSearch2() {
+        assertEquals("[]", dao.search("pro", "8381242").toString());
     }
-    
+
+    @Test
+    public void testSearch3() {
+        assertEquals("[]", dao.search("ybc", "ABCDE").toString());
+    }
+
 }
--- SWP/test/model/DAOTaskTest.java
+++ SWP/test/model/DAOTaskTest.java
@@ -14,21 +14,46 @@ import static org.junit.Assert.*;
  */
 public class DAOTaskTest {
     
-    public DAOTaskTest() {
+    DAOTask dao = new DAOTask();
+    
+    public DAOTaskTest() {        
     }
 
     /**
      * Test of add method, of class DAOTask.
      */
     @Test
-    public void testAdd() {
+    public void testAdd1() {
+        //assertTrue(dao.add("task-demo", 0, "20/06/2022", 0, "QWERT", "project_demo"));
+    }
+    
+    @Test
+    public void testAdd2() {
+        assertFalse(dao.add("task-demo", 0, "20/06/2022", 0, "24344", "project_demo"));
+    }
+    
+    @Test
+    public void testAdd3() {
+        assertFalse(dao.add("task-demo", 0, "20/06/2022", 0, "QWERT", "0650295"));
     }
 
     /**
      * Test of list method, of class DAOTask.
      */
     @Test
-    public void testList() {
+    public void testList1() {
+        assertEquals("[task{name=task-demo, priority=0, deadline=20/06/2022, "
+                + "status=0, assigned=QWERT, project=project_demo}, task{name=task-demo,"
+                + " priority=0, deadline=20/06/2022, status=0, assigned=QWERT,"
+                + " project=project_demo}, task{name=task-demo, priority=0,"
+                + " deadline=20/06/2022, status=0, assigned=QWERT, project=project_demo}, "
+                + "task{name=task-demo, priority=0, deadline=20/06/2022, status=0, "
+                + "assigned=QWERT, project=project_demo}]", dao.list(0).toString());
     }
     
+    @Test
+    public void testList2() {
+        assertEquals("[]", dao.list(1).toString());
+    }
+               
 }

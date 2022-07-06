564258da4ebaecc295e7be8b15e2b7f533e5d97e
nk161690 <khanhndnhe161690@fpt.edu.vn>
2022-07-07 00:34:30 +0700

    ok

--- SWP/src/java/controller/NewPassword.java
+++ SWP/src/java/controller/NewPassword.java
@@ -1,9 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
